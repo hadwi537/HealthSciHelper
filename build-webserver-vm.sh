ca01d0f8e18724fcacfca0636d8b3540137dec74
William of Hadden <williamhadden92@gmail.com>
2022-08-31 17:08:48 +1200

    Added API vm

--- build-webserver-vm.sh
+++ build-webserver-vm.sh
@@ -6,6 +6,7 @@ apt-get install -y apache2 php libapache2-mod-php php-mysql
 # Change VM's webserver's configuration to use the shared folder.
 # See test-website.conf for details
 cp /vagrant/test-website.conf /etc/apache2/sites-available/
+
 #Install custom web config 
 a2ensite test-website
 # Disable default website

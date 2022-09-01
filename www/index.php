d4f97becd2d53ee369c6339203e719b98b3b9cd4
William of Hadden <williamhadden92@gmail.com>
2022-09-01 20:09:01 +1200

    Changed www server and api provisioning

--- www/index.php
+++ www/index.php
@@ -1,46 +1,32 @@
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
-
 <html>
-    <head>
-        <title> Database test page </title>
-        <style>
-            th { text-align: left;}
+<head><title>Webserver test page</title>
+<style>
+th { text-align: left; }
 
-            table, th, td {
-                border: 2px solid grey;
-                border-collapse: collapse
-            }
-            th, td {
-                padding: 0.2em
-            }
-        </style>
-    </head>
+table, th, td {
+  border: 2px solid grey;
+  border-collapse: collapse;
+}
 
-    <body>
-        <h1> Database test page </h1>
-        <p>Showing contents of papers table:</p>
-        
-        <table border="1">
-            <tr><th>Paper code</th><th>Paper name</th></tr>
+th, td {
+  padding: 0.2em;
+}
+</style>
+</head>
 
-            <?php
-            $db_host   = '192.168.2.14';
-            $db_name   = 'fvision';
-            $db_user   = 'webuser';
-            $db_passwd = 'insecure_db_pw';
+<body>
+<h1>Webserver test page.</h1>
 
-            $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+<p>This page demonstrates that the webserver on your VM is generating content.</p>
 
-            $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+<p>You likely now want to <a href="test-database.php">proceed to your webserver's
+database connection testing page</a>. However, note that if there is a network problem reaching the database, the database connection testing page will spend a minute or so waiting before it produces any content.</p>
 
-            $q = $pdo->query("SELECT * FROM papers");
+<p>For your assignment work, your project should begin on this page. The only reason the database testing page was not placed within <kbd>index.php</kbd> was to assist you in debugging any network problems you might be having.</p>
 
-            while($row = $q->fetch()){
-                echo "<tr><td>".$row["code"]."</td><td>".$row["name"]."</td></tr>\n";
-              }
-            ?>
-        </table>
-    </body>
+</body>
 </html>
+
 
 

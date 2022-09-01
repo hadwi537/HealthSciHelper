76c22dfa92f9e9ec6cc9c6d34540bb3a16d9df0a
William of Hadden <williamhadden92@gmail.com>
2022-09-01 20:07:56 +1200

    Reconfigured www page and modified the api provisioning

--- build-api-vm.sh
+++ build-api-vm.sh
@@ -5,6 +5,10 @@ apt-get update
 # apt-get install libapache2-mod-wsgi-py3 python-dev
 
 # Change VM's API's configuration to use the shared folder.
+# We want to copy it across so runs with linux permissions
+# and not a network folder
+cp -r /vagrant/api /home/vagrant/
 
-# then need something here to initalise flask
-# so cd in and run flask app 
+# then move into the api folder
+cd api
+
